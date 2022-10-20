210627e7ee0a94a716c85bc16ad53a38a9ef561f
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-20 18:24:28 +0300

    Variable welcome

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-console.log(true); // boolean
-console.log('This is text'); // string
-console.log(50); // number
-console.log(undefined);
-console.log(null);
-console.log(5n); // bigint
+/*
+Переменная
+Змінна
+Variable
+*/
+
+/*
+Створення змінної
+
+ключове_слово ім'я_змінної
+
+var -- deprecated 
+
+let - CD-RW
+const - CD-R
+
+*/
+
+let box; // Оголошення змінної (Обьявление переменной) (Variable Declaration)
+
+/* Оператор присвоєння = */
+ 
+box = 3; // Присвоєння (Присваивание) (Assign)
+console.log(box);
+
+let box2 = "Hello";
+
+box2 = 10;
+console.log(box2);
+
+
+
+
+/*
+
+Правила іменування
+
+*/
+
+let important variable = 0; - неправильно
+
+/*
+CamelCase
+*/
+
+let importantVariable;
+importantVariable = 0;
+
+let a = 6;
+
+// Ім'я змінної має пояснювати що в ній зберігається
+
+let data; // bad practice
+let value; // bad practice
+
+let 123value; - Це заборонено!
+
+let _value; private field
+let #value; private field
+
