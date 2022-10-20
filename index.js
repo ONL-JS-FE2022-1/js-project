badf7e228db62c019f9c26d125dd11d6cc062589
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-20 18:42:36 +0300

    let && const difference

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
-/*
-Переменная
-Змінна
-Variable
-*/
+let box1 = 'Hello';
 
-/*
-Створення змінної
+const box2 = true;
 
-ключове_слово ім'я_змінної
+// console.log(box1);
+// console.log(box2);
 
-var -- deprecated 
+box1 = 10;
+console.log(box1);
 
-let - CD-RW
-const - CD-R
-
-*/
-
-let box; // Оголошення змінної (Обьявление переменной) (Variable Declaration)
-
-/* Оператор присвоєння = */
- 
-box = 3; // Присвоєння (Присваивание) (Assign)
-console.log(box);
-
-let box2 = "Hello";
-
-box2 = 10;
+// box2 = 15; константи перевизначати заборонено!
 console.log(box2);
 
-
-
-
-/*
-
-Правила іменування
-
-*/
-
-let important variable = 0; - неправильно
-
-/*
-CamelCase
-*/
-
-let importantVariable;
-importantVariable = 0;
-
-let a = 6;
-
-// Ім'я змінної має пояснювати що в ній зберігається
-
-let data; // bad practice
-let value; // bad practice
-
-let 123value; - Це заборонено!
-
-let _value; private field
-let #value; private field
-
+const box = 'USER_AVATAR_URL';
+box = 123;
+let box3;
