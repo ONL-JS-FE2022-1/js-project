d7e75c6c7cf959c199e663a066b363afcdac73bd
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-26 19:38:55 +0300

    HOF (Vasya) example

--- index.js
+++ index.js
@@ -1,11 +1,118 @@
-/* Side effect - Побічний ефект */
+function ymivanie() {
+    // ............
+    console.log('ymivanie');
+}
 
-console.log('Hello');
-alert();
-fetch(); // Запити на сервер або будь-які міжмережеві запити
+function chistkaZybov() {
+    // ............
+    console.log('chistka zybov');
+}
 
-/*
-Чиста функція (pure function):
-1. Детермінована функція
-2. Не має побічних ефектів
-*/
+function yxodZaKozhey() {
+    // ............
+    console.log('yxodZaKoshey');
+}
+
+function coding() {
+    // ............
+    console.log('coding');
+}
+
+function sleep() {
+    // ............
+    console.log('sleep')
+}
+
+function goToWork() {
+    // ............
+    console.log('goToWork')
+}
+
+function goToHome() {
+    // ............
+    console.log('goToHome')
+}
+
+function zavtrak() {
+    // ............
+    console.log('zavtrak')
+}
+
+function education() {
+    // ............
+    console.log('education')
+}
+
+function obed() {
+    // ............
+    console.log('obed')
+}
+
+function yzhin() {
+    // ............
+    console.log('yzhin')
+}
+
+function progulka() {
+    // ............
+    console.log('progulka')
+}
+
+function vanna() {
+    // ............
+    console.log('vanna')
+}
+
+function todo(timeOfDay) { // HOF
+    switch(timeOfDay) {
+        case 'morning': {
+            console.group('morning');
+
+            ymivanie();
+            chistkaZybov();
+            yxodZaKozhey();
+            progulka();
+            zavtrak();
+
+            console.groupEnd();
+
+            break;
+        }
+        case 'obed': {
+            console.group('obed');
+
+            goToWork();
+            coding();
+            obed();
+            coding();
+            progulka();
+            goToHome();
+
+            console.groupEnd();
+
+            break;
+        }
+        case 'evening': {
+            console.group('evening');
+
+            education();
+            yzhin();
+            coding();
+            vanna();
+            ymivanie();
+            yxodZaKozhey();
+            sleep();
+
+            console.groupEnd();
+
+            break;
+        }
+        default: {
+            console.log('Вася не в духе!. Введите нормальное время дня!!!')
+        } 
+    }
+}
+
+todo('morning');
+todo('obed');
+todo('evening');
