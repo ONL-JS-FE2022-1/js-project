bc45f861fc3984745ad88c3b5c6593d17527a82d
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-26 19:16:09 +0300

    Pure function

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-function sum(a, b) { // детермінована функція
-    return a + b;
-}
+/* Side effect - Побічний ефект */
 
-sum(2, 2); // 4
-sum(2, 2); // 4
-
-/*
-Детермінована функція - це функція, яка при одних і тих же аргументах ЗАВЖДИ повертає один і той самий результат
-*/
-
-let variable = 5;
-function addToNumberSomeVariable(a) { // недетермінована функція
-    return a + variable;
-}
-
-addToNumberSomeVariable(5); // 10
-
-variable = 20;
-
-addToNumberSomeVariable(5); // 25
+console.log('Hello');
+alert();
