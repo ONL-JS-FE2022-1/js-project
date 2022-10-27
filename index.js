54d8203783eee108255f490f3ed549837cdcb9f6
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-27 19:06:56 +0300

    Objects

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-/*
-Objects
+const cat = {
+    age: 2
+} // Літеральне створення об'єкта
 
-Об'єкти мають:
-- властивості
-- методи
-
-*/
-
-let obj = {
-  color: 'red',
-  fontSize: 2
-}
-
-let bulb = {
-  producer: 'Philips',
-  lightness: 200,
-  power: '200W',
-  cap: 'small',
-  on: function() {
-    return 'The light on!'
-  }
-}
-
-console.log(bulb.lightness);
-console.log(bulb.on());
+const cat2 = Object(); // Функція-конструктор
+const cat3 = new Object(); // Функція-конструктор
