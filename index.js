eb7c6c0758e6fff5d3af0c8cabda87644b2da702
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-27 19:39:57 +0300

    Cat crud

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const cat = {
-    age: 2
-} // Літеральне створення об'єкта
+    name: 'Murzik',
+    color: 'Red',
+    breed: 'Dvorovyi osobluvij',
+    age: 2,
+    weight: 5,
+    sex: 'm',
+    eat: function() {
+        return 'I am eating'
+    },
+    sleep: function() {
+        return 'I am sleeping'
+    }
+}
 
-const cat2 = Object(); // Функція-конструктор
-const cat3 = new Object(); // Функція-конструктор
+cat.sleep = undefined; // BAD PRACTICE
+
+delete cat.sleep; // Good practice
+
+// Adding properties
+cat.girlfriend = 'Murka';
+
+/*
+Визначте у себе кота, опишіть його властивості і декілька методів - бігти, спати, нявкати
+*/
