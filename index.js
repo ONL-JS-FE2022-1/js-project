bb73bc2bc92aedf2fbdd382ec2a78de3425108d0
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-27 20:17:00 +0300

    Function-constructors

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const cat = {
-    name: 'Murzik',
-    color: 'Red',
-    breed: 'Dvorovyi osobluvij',
-    age: 2,
-    weight: 5,
-    sex: 'm',
-    eat: function() {
-        return 'I am eating'
-    },
-    sleep: function() {
-        return 'I am sleeping'
+// Функція-конструктор
+
+function Cat(name, color, breed, age) { // Угода: функції-конструктори називаються з великої літери
+    this.name = name; // this - посилання на ось цього новостворенного кота
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+    this.run = function() {
+        return this.name + ' is running';
     }
 }
 
-cat.sleep = undefined; // BAD PRACTICE
+const cat1 = new Cat('Bublik', 'red', null, 2);
+const cat2 = new Cat('Murz', 'white', 'Siam', 4);
+const cat3 = new Cat('Test');
+cat3.age = 3;
+cat3.boyfriend = 'Tyzik';
 
-delete cat.sleep; // Good practice
+/*
+Функція-конструктор Country, яка приймає в якості аргументів:
+- назва країни
+- популяція (population)
+- площа (area)
+- плотність населення (density): популяція цієї ж країни / площу цієї ж країни
 
-// Adding properties
-cat.girlfriend = {
-    name: 'Murka',
-    color: 'black',
-    age: 1
-};
-
+Зробіть декілька країн
+*/
