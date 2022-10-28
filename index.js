bec84f138522b90449dc18ff0b20e9abfffa314e
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-28 19:48:16 +0300

    Another example of how computing properties work

--- index.js
+++ index.js
@@ -1,48 +1,12 @@
-/* Обчислювальні властивості */
+/* 
 
-const user = {
-    'name': 'John', // Ключ 'name', значення - 'John'
-    'favorite color': 'red',
-    0: 'value'
+Задача: вивести користувачу привітання, залежно від значення, яке він ввів
+
+*/
+
+const greets = {
+    15: 'Hi',
+    17: 'Ya, whats up',
+    18: 'Hello',
+    30: 'Good morning'
 }
-
-/* Ключ в об'єкті може бути рядок або тип даних Symbol */
-
-
-// . - оператор доступу за ім'м властивості
-
-// user.name // 'John'
-// user.'favorite color' // syntax error
-// user.0 // syntax error
-
-// [] - Оператор доступу до обчислювальної властивості (Computed property)
-
-let name = 'Sam';
-
-user[name]; // === user.Sam
-user['favorite color'] // red
-user[0]; // 'value'
-
-
-/////////////////////////////////////////////
-
-let key = 'a';
-
-
-const obj = {
-    a: 5
-}
-
-obj.key // undefined
-console.log(obj[key]); // 5
-
-
-//////////////////////////////////////////////
-
-const someObj = {
-    2: 10
-}
-
-// someObj.2 // syntax error
-someObj[2] // 10
-someObj['2']; // 10
