d124a661890bef838a44a97e0b5fc404e4b912e1
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-28 20:32:21 +0300

    Loop: for-in

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-
-const todo = {
-    Mon: 'drive lesson',
-    Tue: 'dantist visit',
-    Wed: 'go to party',
-    Thu: 'chill',
-    Fri: 'hard work',
-    Sat: 'go to park'
+const user = {
+    name: 'John',
+    lastName: 'Doe',
+    age: 20,
+    'favorite color': 'red',
+    movie: 'Blue moon',
+    avatar: 'http://.....'
 }
 
-/* 'key' in obj */
+// for ... in
 
-console.log('Mon' in todo);
+// НЕ ЦЕЙ for(;;)
 
-const obj2 = {
-    key1: undefined
+for (let key in user) {
+    console.log(`${key} ---- ${user[key]}`); // key ---- value
 }
-
-let key1 = 'val';
-
-console.log(key1 in obj2); // 'val' in obj2
-console.log('key1' in obj2); // key1 in obj2
-
-function greeting(user) {
-    return `Hello, ${'name' in user ? user.name : 'Anonym'}`;
-}
-
-const user1 = {
-    name: 'Vasya'
-}
-
-const user2 = {};
-
-console.log(greeting(user1)); // Hello, Vasya
-console.log(greeting(user2)); // Hello, Anonym
