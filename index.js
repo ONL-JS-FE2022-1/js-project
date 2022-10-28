22ea3445ae24063d06e8cc76ad03d079ced67ea5
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-28 19:26:13 +0300

    Computed Property

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-//alert('2+2=' + (2+2));
+/* Обчислювальні властивості */
 
-/*
-'Привіт'
-"Привіт"
-`Привіт`
-*/
-
-//let name = 'Вася';
-//alert(`Привіт, ${name}`);
-//alert(`2+2=${2+2}`);
-//alert('2+2=${2+2}');
-
-
-
-
-// const one = Number(prompt('One number'));
-// const two = Number(prompt('Second number'));
-
-//alert(''+one+'+'+two+'='+(one+two));
-// 2+2=4
-
-// alert(`${one} + ${two} = ${one + two}`);
-
-
-/////////////////////////////////////////////////////////////////////
-
-// У нас є функція
-function greetingUser(user) {
-    return `Hey ${user.name} ${user.surname}. You will be ${user.age + 1} next year.`
-}
-
-// та у нас є об'єкт
 const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 15
+    'name': 'John', // Ключ 'name', значення - 'John'
+    'favorite color': 'red',
+    0: 'value'
 }
 
-console.log(greetingUser(user));
+/* Ключ в об'єкті може бути рядок або тип даних Symbol */
 
-// написати функцію таким чином, щоб вона привітала користувача та вказала, скільки йому років має виповнитись у наступному році
+
+// . - оператор доступу за ім'м властивості
+
+// user.name // 'John'
+// user.'favorite color' // syntax error
+// user.0 // syntax error
+
+// [] - Оператор доступу до обчислювальної властивості (Computed property)
+
+let name = 'Sam';
+
+user[name]; // === user.Sam
+user['favorite color'] // red
+user[0]; // 'value'
+
+
+/////////////////////////////////////////////
+
+let key = 'a';
+
+
+const obj = {
+    a: 5
+}
+
+obj.key // undefined
+console.log(obj[key]); // 5
+
+
+//////////////////////////////////////////////
+
+const someObj = {
+    2: 10
+}
+
+// someObj.2 // syntax error
+someObj[2] // 10
+someObj['2']; // 10
