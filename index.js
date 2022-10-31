3e4875ddb8f5fcd64c8c82fbebcc08904379008e
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-10-31 18:25:05 +0200

    Array welcome

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const user = {
-    name: 'John',
-    lastName: 'Doe',
-    age: 20,
-    'favorite color': 'red',
-    movie: 'Blue moon',
-    avatar: 'http://.....'
+let userTel1 = '3805465465465';
+let userTel2 = '3805165161565';
+let userTel3 = '4654351321232';
+
+const userTels = {
+    0: '3805465465465',
+    1: '3805165161565',
+    2: '4654351321232'
 }
 
-// for ... in
+// Array
 
-// НЕ ЦЕЙ for(;;)
+const arr = [2, 4, 6, 7];
+const arr2 = new Array(2, 4, 6, 7);
 
-for (let key in user) {
-    console.log(`${key} ---- ${user[key]}`); // key ---- value
+///////
+
+
+// вивести значення всіх елементів масиву array на єкран
+const array = [7, 3, 5, 2];
+array[0];
+array[1];
+array[2];
+array[3];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(
+        array[i]
+    );
 }
