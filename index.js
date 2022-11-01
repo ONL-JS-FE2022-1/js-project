4b23ae10254f1b28085cf68767135f5f482888b0
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-01 20:48:36 +0200

    ForEach (task 3) solution

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
-// function square(a) {
-//     return a*a;
-// }
+/* TASK 3
 
-const array = [1,2,3,4,5];
+Дан масив об'єктів
+const users = [
+    {
+        name: 'John',
+        lastName: 'Doe',
+        age: 18,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jane',
+        lastName: 'Doe',
+        age: 20,
+        email: 'meil@com'
+    },
+    {
+        name: 'Josh',
+        lastName: 'Doe',
+        age: 17,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jake',
+        lastName: 'Doe',
+        age: 21,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jackson',
+        lastName: 'Doe',
+        age: 17,
+        email: 'meil@com'
+    },
+]
 
-// console.log(array.map(square));
+Всім користувачам додати поле isSubscribed = false
 
-const newArray = array.map((item) => {
-    return item ** 2;
-})
+*/
+
+const users = [
+    {
+        name: 'John',
+        lastName: 'Doe',
+        age: 18,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jane',
+        lastName: 'Doe',
+        age: 20,
+        email: 'meil@com'
+    },
+    {
+        name: 'Josh',
+        lastName: 'Doe',
+        age: 17,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jake',
+        lastName: 'Doe',
+        age: 21,
+        email: 'meil@com'
+    },
+    {
+        name: 'Jackson',
+        lastName: 'Doe',
+        age: 17,
+        email: 'meil@com'
+    },
+]
+
+// TASK 3
+// variant 1
+function addSubscribeField(item) {
+    item.isSubscribed = false;
+}
+users.forEach(addSubscribeField);
+
+// variant 2
+// users.forEach((item) => {
+//     item.isSubscribed = false;
+// })
