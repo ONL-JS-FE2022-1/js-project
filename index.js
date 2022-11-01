1661632d9e266d05b11159709d8f30f42d8eac33
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-01 20:25:21 +0200

    Map method

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // function square(a) {
-//     console.log(a*a);
+//     return a*a;
 // }
 
 const array = [1,2,3,4,5];
 
-// console.log(array.forEach(square)); // return undefined
+// console.log(array.map(square));
 
-array.forEach((item, index, array) => {
-    console.log(`елмент: ${item} квадрат: ${item ** 2} индекс: ${index} массив: ${array}`);
+const newArray = array.map((item) => {
+    return item ** 2;
 })
