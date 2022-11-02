8668baa5379f9a28bec9154eb7ea211b7f5af540
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-02 19:07:20 +0200

    Chaining

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-function t() {
-console.log(arguments);
-console.log(arguments.length);
-}
-
-console.log(t);
-console.dir(t); // консолить об'єкт з його властивостями та методами
-
-t(1,2,3,4,5);
-
-/* function sum(a, b) {
-    const arrayArgs = Array.from(arguments);
-    console.log(arrayArgs);
-    return a + b;
-} */
-
 /*
-Напишіть функцію, яка повертає суму БУДЬ-ЯКОЇ кількості переданих їй аргументів
-
-sum(2,3) // 5
-sum(4, 7, 2, 1) // 14
+новий масив, кожен елемент якого = елемент зі старого зі старого масиву * 2
 */
 
-function sum() {
-    const arrayArgs = Array.from(arguments); // нормальний масив з об'єкту arguments
+const array = [2,3,4,5,1,2];
 
-    let sum = 0;
-    for(let i = 0; i < arrayArgs.length; i++) {
-        sum += arrayArgs[i];
-    }
-    return sum;
-}
+const newArray = array.map((item) => {
+    return item * 2;
+}).reverse().join(','); // chaining
 
-console.log(sum(2, 6, 23, 65, 123, 3432));
-console.log(sum(1,2,3));
-console.log(sum(1,2,3,4,5));
+console.log(newArray);
