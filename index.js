e1f4d697629d0d48f953d6db2dc44c27223a89ea
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-02 18:09:13 +0200

    ForEach + map tasks solutions

--- index.js
+++ index.js
@@ -76,9 +76,48 @@ const users = [
 function addSubscribeField(item) {
     item.isSubscribed = false;
 }
-users.forEach(addSubscribeField);
+//users.forEach(addSubscribeField);
 
 // variant 2
 // users.forEach((item) => {
 //     item.isSubscribed = false;
 // })
+
+
+/*
+Задача 4
+
+Дан масив const arr = [2, 44, 11, 234, 8, 2, 4, 1]
+
+Зробити новий масив, всі елементи якого = елемент зі старого масиву + 100
+*/
+
+const arr = [2, 44, 11, 234, 8, 2, 4, 1];
+const plus100 = arr.map((item) => {
+    return item + 100;
+});
+
+
+/*
+Задача 5
+
+На основі масиву об'єктів з задачі 3.
+Створити новий масив користувачів, об'єкти мають бути у такому вигляді:
+{
+    fullName: 'John Doe',
+    age: ....,
+    email: ...
+}
+
+Тобто потрібно якось перетворити два поля name та lastName у одне поле - fullName
+*/
+
+function newUserObject(user) {
+    return {
+        fullName: `${user.name} ${user.lastName}`,
+        age: user.age,
+        email: user.email
+    }
+}
+
+const newUsersArray = users.map(newUserObject);
