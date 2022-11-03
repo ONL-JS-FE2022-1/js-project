857cf0d7d973e180d1545354546cf4f94fb465ae
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-03 19:40:56 +0200

    MyArray

--- index.js
+++ index.js
@@ -1,97 +1,44 @@
-const catArray = [
-    {
-        name: 'Murzik',
-        color: 'black',
-        weight: 3,
-        age: 2
-    },
-    {
-        name: 'Murka',
-        color: 'grey',
-        weight: 5,
-        age: 3
-    },
-    {
-        name: 'Barsik',
-        color: 'red',
-        weight: 2,
-        age: 1
-    },
-    {
-        name: 'Cookie',
-        color: 'brown',
-        weight: 5,
-        age: 2
-    },
-    {
-        name: 'Stus',
-        color: 'grey',
-        weight: 2,
-        age: 5
-    },
-    {
-        name: 'Fagot',
-        color: 'black',
-        weight: 5,
-        age: 3
-    },
-    {
-        name: 'Kit',
-        color: 'pink',
-        weight: 2,
-        age: 1
-    },
-]
+function MyArray() {
+    this.length = 0;
 
-// 1. Відсортувати котів за вагою на збільшення
-// catArray.sort((cat1, cat2) => {
-//     if(cat1.weight < cat2.weight) {
-//         return -1;
-//     } else {
-//         return 1;
-//     }
-// });
+    this.push = function() {
+        // push - добавляет єлемент, которій нам передают в конец массива
+        // изначально, длина массива (this.length = 0)
+        // куда добавлять? = что нам нужно добавить?
+        // this[this.length] = value;
+        // this.length++;
 
-/* returning:
--1 -> cat1, cat2
-0 -> всі на місці
-1 -> cat2, cat1
-*/
+        // this[this.length] = value; // this[key] = value
+        // this.length++;
+        // return this.length;
 
-/*
-Задача 4
 
-Відсортувати котів від найстаршого до наймолодшого.
-Можете використовувати для вирішення цієї задачі ту форму, яку я показав вам на парі, а можете скористатись такою формою:
 
-Для числового сравнения, вместо строкового, функция сравнения может просто вычитать b из a. Следующая функция будет сортировать массив по возрастанию:
 
-function compareNumbers(a, b) {
-  return a - b;
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
+        return this.length;
+    }
+
+    this.pop = function() {
+        if(this.length > 0) {
+            let lastItem = this[this.length - 1];
+            delete this[this.length - 1];
+            this.length--;
+            return lastItem;
+        } 
+    }
 }
-*/
 
-// catArray.sort((cat1, cat2) => {
-//     return cat2.age - cat1.age;
-// });
+const arr = new MyArray();
 
 /*
-Задача 5
+Перепишіть push таким чином, щоб push міг приймати та додавав до масиву будь-яку перредану кількість елементів
+Якщо ви знаєте цей матеріал, то оператор розширення не використовуйте =)
 
-Відсіяти всіх котів окрім чорних.
+arr.push(3,2,1,3,4,5555,2,1);
+
+#arguments
 */
-
-// const blackCatArray = catArray.filter((cat) => {
-//     return cat.color === 'black';
-// });
-
-/*
-Задача 6
-
-Створити новий масив, який складається з імен котів.
-*/
-
-const catNames = catArray.map((cat) => {
-    return cat.name;
-})
-
