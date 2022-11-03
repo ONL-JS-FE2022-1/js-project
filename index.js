201f96746a3085080cc5ec4c81736385d77c78ea
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-03 18:13:47 +0200

    JS arrays home task resolve

--- index.js
+++ index.js
@@ -44,16 +44,54 @@ const catArray = [
 ]
 
 // 1. Відсортувати котів за вагою на збільшення
-catArray.sort((cat1, cat2) => {
-    if(cat1.weight < cat2.weight) {
-        return -1;
-    } else {
-        return 1;
-    }
-});
+// catArray.sort((cat1, cat2) => {
+//     if(cat1.weight < cat2.weight) {
+//         return -1;
+//     } else {
+//         return 1;
+//     }
+// });
 
 /* returning:
 -1 -> cat1, cat2
 0 -> всі на місці
 1 -> cat2, cat1
 */
+
+/*
+Задача 4
+
+Відсортувати котів від найстаршого до наймолодшого.
+Можете використовувати для вирішення цієї задачі ту форму, яку я показав вам на парі, а можете скористатись такою формою:
+
+Для числового сравнения, вместо строкового, функция сравнения может просто вычитать b из a. Следующая функция будет сортировать массив по возрастанию:
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+*/
+
+// catArray.sort((cat1, cat2) => {
+//     return cat2.age - cat1.age;
+// });
+
+/*
+Задача 5
+
+Відсіяти всіх котів окрім чорних.
+*/
+
+// const blackCatArray = catArray.filter((cat) => {
+//     return cat.color === 'black';
+// });
+
+/*
+Задача 6
+
+Створити новий масив, який складається з імен котів.
+*/
+
+const catNames = catArray.map((cat) => {
+    return cat.name;
+})
+
