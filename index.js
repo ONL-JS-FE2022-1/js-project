61be345e733658fd4659f16591c77b0cefa7cfb9
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-03 20:20:01 +0200

    .prototype

--- index.js
+++ index.js
@@ -1,75 +1,26 @@
-function MyArray() {
+function MyArray() { // функція-конструктор масивів
     this.length = 0;
+}
 
+function myProtoArray() {
     this.push = function() {
-        // push - добавляет єлемент, которій нам передают в конец массива
-        // изначально, длина массива (this.length = 0)
-        // куда добавлять? = что нам нужно добавить?
-        // this[this.length] = value;
-        // this.length++;
-
-        // this[this.length] = value; // this[key] = value
-        // this.length++;
-        // return this.length;
-
-
-
-
         for(let i = 0; i < arguments.length; i++) {
             this[this.length] = arguments[i];
             this.length++;
         }
         return this.length;
-    }
-
+    },
     this.pop = function() {
         if(this.length > 0) {
             let lastItem = this[this.length - 1];
             delete this[this.length - 1];
             this.length--;
             return lastItem;
-        } 
+        }
     }
 }
+
+MyArray.prototype = new myProtoArray;
+
 
 const arr = new MyArray();
-
-/*
-Перепишіть push таким чином, щоб push міг приймати та додавав до масиву будь-яку перредану кількість елементів
-Якщо ви знаєте цей матеріал, то оператор розширення не використовуйте =)
-
-arr.push(3,2,1,3,4,5555,2,1);
-
-#arguments
-*/
-
-
-
-
-
-
-const cat = {
-    name: 'Barsik',
-    color: 'red',
-    age: 1
-};
-
-const cat2 = {
-    name: 'Murzik',
-    color: 'black',
-    age: 5
-};
-
-const catMethods = {
-    run: function() {
-        console.log(`${this.name} is running!`);
-    },
-    meow: function() {
-        console.log(`${this.name} said meow!`);
-    }
-}
-
-cat.__proto__ = catMethods;
-cat2.__proto__ = catMethods;
-
-cat.run() // this у методі run буде вказувати на об'єкт який йде до крапки
