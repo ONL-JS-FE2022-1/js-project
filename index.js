634a8538dbbde4f51b215d2751417949c5a8066c
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-04 18:32:10 +0200

    Three types of function

--- index.js
+++ index.js
@@ -1,70 +1,21 @@
-/*
-LADDER
+'use strict';
 
-Задача: створити функцію-конструктор для сходів
-Об'єкт має властивість
-currentStair // сходинка, на якій ми зараз знаходимось. Початково = 0
+// this - це контекст
 
-Має методи:
-up() - піднімає на сходинку вище
-down() - опускає на сходинку нижче
-showStair() - показує, на якій сходинці ми зараз знаходимось
-*/
-
-function Ladder() { // функция-конструктор
-    this.currentStair = 0;
+function test() { // Function Declaration
+    console.log(this); // this вказує на функцію
 }
+test();
 
-function LadderProto() { // функция-конструктор
-    this.up = function() {
-        console.log('up')
-        this.currentStair++;
-        return this;
-    }
-
-    this.down = function() {
-        console.log('down')
-        this.currentStair--;
-        return this;
-    }
-
-    this.showStair = function() {
-        console.log('currentStair')
-        return this.currentStair;
-    }
+const test2 = function() { // Function Expression
+    console.log(this); // this вказує на функцію
 }
+test2();
 
-Ladder.prototype = new LadderProto();
+console.log(this); // this - вказує на глобальний об'єкт window
 
-const ladder = new Ladder();
-// ladder.up().up().down().up().showStair();
 
-/*
-
-Зробити функцію-конструктор об'єкту Accumulator, який приймає певне початкове значення при створенні
-
-new Accumulator(startingValue)
-має властивість 
-value
-
-має метод read(), який за допомогою prompt приймає від користувача чтсло та додає його до value, повертає значення поточного value
-(перевірка на число)
-*/
-
-function Accumulator(startingValue = 0) {
-    this.value = startingValue;
+const arrow = () => { // Arrow Function не мають власного контексту
+    console.log(this);
 }
-
-function AccumulatorProto() {
-    this.read = function() {
-        const userValue = Number(prompt('Введіть число'));
-
-        if(Number.isNaN(userValue)) {
-            console.log('Це не число!');
-        } else {
-            this.value += userValue;
-        }
-    }
-}
-
-Accumulator.prototype = new AccumulatorProto();
+arrow();
