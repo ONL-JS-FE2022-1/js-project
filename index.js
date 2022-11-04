f9f332aae5f5ea767a23281c612b77daea1f958c
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-04 20:06:56 +0200

    Rest-n-Spread

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ sum(1,2,3,4,5,67,6); // 3
 const arrowSum = (...restArray) => restArray.reduce((accum, current) => accum+current);
 
 console.log(arrowSum(5,5,5,5));
+
+// spread
+// [] -> .., .., .., ...,
+
+const numbers = [1,2,3,4,5];
+sum(numbers); // не спрацює, бо весь масив запишеться в аргумент a
+sum(numbers[0], numbers[1]); // теж не варіант - довго
+sum(...numbers); // правильний варіант 
+
+
+///////////////////////
+
+const arr1 = [1,2,3,4,5]
+const arr2 = [10,11,12,13,14,15]
+
+// Зклейте два масиви в один, не використовуючи concat
+
+const concatArray = [...arr1, ...arr2];
