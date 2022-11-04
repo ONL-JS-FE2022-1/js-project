23eaf187ff81957fd8d01601854c038bc135f4e9
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-04 18:58:59 +0200

    Arrow Function Rules

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 'use strict';
 
-const newspaper = {
-    title: 'Header',
-    articles: [
-        {
-            author: '1 author',
-            date: '04-11-2022',
-            text: 'lorem'
-        },
-        {
-            author: '2 author',
-            date: '04-11-2022',
-            text: 'lorem'
-        },
-        {
-            author: '3 author',
-            date: '04-11-2022',
-            text: 'lorem'
-        },
-        {
-            author: '4 author',
-            date: '04-11-2022',
-            text: 'lorem'
-        }
-    ],
-    showArticles: function() {
-        this.articles.forEach((article, index) => {
-            console.log(`${this.title} ${index} - ${article.author}`); // Втрата контексту this
-        })
-    }
+function sum(a, b) {
+    return a + b;
 }
 
-newspaper.showArticles();
+const arrow = (a, b) => a + b; // a + b === {return a+b}
+
+const pow = x => x ** 2; // якщо 1 аргумент, то () можна опустити
+
+const SuperFunc = () => {
+    this.value = 'hello!';
+}
+
+const t1 = function() {
+    console.log(arguments);
+}
+
+const t2 = () => {
+    console.log(arguments); // Arrow Function не має arguments
+}
+
+///////
+
+/*
+
+1. Стрілочна функція не має власного контексту (її this буде вказувати на середовище її виконання)
+2. Стрілочна функція не може бути функцією-конструктором
+3. Стрілочна функція не має колекції arguments
+
+*/
+
