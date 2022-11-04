448f6272c530ff232393d1f643b6dd53e72c9485
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-04 18:10:30 +0200

    Accumulator task resolve

--- index.js
+++ index.js
@@ -38,3 +38,33 @@ Ladder.prototype = new LadderProto();
 
 const ladder = new Ladder();
 // ladder.up().up().down().up().showStair();
+
+/*
+
+Зробити функцію-конструктор об'єкту Accumulator, який приймає певне початкове значення при створенні
+
+new Accumulator(startingValue)
+має властивість 
+value
+
+має метод read(), який за допомогою prompt приймає від користувача чтсло та додає його до value, повертає значення поточного value
+(перевірка на число)
+*/
+
+function Accumulator(startingValue = 0) {
+    this.value = startingValue;
+}
+
+function AccumulatorProto() {
+    this.read = function() {
+        const userValue = Number(prompt('Введіть число'));
+
+        if(Number.isNaN(userValue)) {
+            console.log('Це не число!');
+        } else {
+            this.value += userValue;
+        }
+    }
+}
+
+Accumulator.prototype = new AccumulatorProto();
