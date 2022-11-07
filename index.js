2a44811df4adddbcbbc12b4a352b9ed795f85287
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-07 20:47:22 +0200

    Add checkSpam task solution

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
-/*
-Задача.
+/* 
+Задача 1
 
-Написати функцію, яка робить кожне слово у переданому рядку з великої літери
+Таска:
+Написати функція truncate, яка приймає в якості аргументів рядок (str) і довжину (strLength).
+Якщо довжина > strLength, то врізати рядок до цієї довжини і в кінці додати "..."
+Якщо рядок менший, нічого не робити.
+
+truncate('hello', 2); // he...
+truncate('hello', 10); // hello
 
 */
 
-function eachWordInUpper(str) {
-    const wordArray = str.split(' ');
-    const resultWordArray = [];
-
-    for(let i = 0; i < wordArray.length; i++) {
-        let word = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1);
-        resultWordArray.push(word);
+function truncate(str, strLength) {
+    if(str.length <= strLength) {
+        return str;
     }
-    return resultWordArray.join(' ');
+    
+    const newStr = str.slice(0, strLength);
+    return newStr + '...';
 }
 
-console.log(eachWordInUpper('hello to you'));
+/*
+
+Задача 2
+
+Написати функцію checkSpam, яка повертає true, якщо переданий рядок містить слова 'xxx' або 'viagra', інакше false.
+
+checkSpam('buy VIagRA now'); // true
+checkSpam('free xxxxxxxxx'); // true
+checkSpam('innocent rabbit'); // false
+
+*/
+
+// variant 1
+function checkSpam(str) {
+    const lowStr = str.toLowerCase();
+    return lowStr.includes('viagra') || lowStr.includes('xxx');
+}
+
+// variant 2
+function checkSpam2(str) {
+    const spamArray = ['viagra', 'xxx', 'drugs'];
+
+    let flag = false;
+
+    for(let i = 0; i < spamArray.length; i++) {
+        if(str.toLowerCase().includes(spamArray[i])) {
+            flag = true;
+        }
+    }
+
+    return flag;
+}
+
+
+
+
+// Домашня практика
+
+/*
+Задача 3.
+
+Написати функцію яка перевіряє, чи є переданий рядок паліндромом.
+Не зважаючи на регістр.
+Паліндром - це коли рядок з обох боків читається однаково.
+
+Anna - паліндром
+Papa - не паліднром
+Namman - паліндром
+*/
+
+
+
+/*
+Задача 4
+
+Написати функцію, яка приймає рядок і повертає кількість голосних літер, що містяться в рядку.
+(мова про латинську абетку)
+
+Голосні: ['a', 'e', 'i', 'o', 'u'];
+
+*/
