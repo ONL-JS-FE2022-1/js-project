7cdff21f33ee21a554240467ace4cde711f2b648
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-08 19:40:29 +0200

    Recursion

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-// Date
+// Recursion
 
-const time1 = new Date();
+// pow(x, n); 
+// pow(2, 2); 4
+// pow(2, 4); 16
+// pow(2, 3); === 2 * 2 * 2 = 8
 
-let sum = 0;
-for(let i = 0; i < 50000; i++) {
-    sum += i;
+// Ітераційний підхід (ітераційне мислення)
+// function pow(x, n) {
+//     let result = 1;
+
+//     // множимо result на x n разів в циклі
+//     for(let i = 0; i < n; i++) {
+//         result *= x;
+//     }
+
+//     return result;
+// }
+
+// console.log(pow(3, 4)); // 81
+
+// Рекурсивний підхід (рекурсивне мислення)
+function pow(x, n) {
+    if(n === 1) {
+        return x;
+    } else {
+        return x * pow(x, n - 1);
+    }
 }
-console.log(sum);
+console.log(pow(2, 4)); // 81
 
-const time2 = new Date();
+/*
+                   if n===1  = x
+             /
+pow(x, n) = 
+             \
+                   else      = x * pow(x, n - 1) 
+*/
 
-console.log(time2.getTime() - time1.getTime());
+// x^n = x * x^(n-1)
+
+
