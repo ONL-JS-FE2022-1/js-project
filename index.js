c66fa016ef6b66675149aef836346101fb749029
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-08 18:48:22 +0200

    Date

--- index.js
+++ index.js
@@ -1,98 +1,13 @@
-/* 
-Задача 1
+// Date
 
-Таска:
-Написати функція truncate, яка приймає в якості аргументів рядок (str) і довжину (strLength).
-Якщо довжина > strLength, то врізати рядок до цієї довжини і в кінці додати "..."
-Якщо рядок менший, нічого не робити.
+const time1 = new Date();
 
-truncate('hello', 2); // he...
-truncate('hello', 10); // hello
-
-*/
-
-function truncate(str, strLength) {
-    if(str.length <= strLength) {
-        return str;
-    }
-    
-    const newStr = str.slice(0, strLength);
-    return newStr + '...';
+let sum = 0;
+for(let i = 0; i < 50000; i++) {
+    sum += i;
 }
+console.log(sum);
 
-/*
+const time2 = new Date();
 
-Задача 2
-
-Написати функцію checkSpam, яка повертає true, якщо переданий рядок містить слова 'xxx' або 'viagra', інакше false.
-
-checkSpam('buy VIagRA now'); // true
-checkSpam('free xxxxxxxxx'); // true
-checkSpam('innocent rabbit'); // false
-
-*/
-
-// variant 1
-function checkSpam(str) {
-    const lowStr = str.toLowerCase();
-    return lowStr.includes('viagra') || lowStr.includes('xxx');
-}
-
-// variant 2
-function checkSpam2(str) {
-    const spamArray = ['viagra', 'xxx', 'drugs'];
-
-    let flag = false;
-
-    for(let i = 0; i < spamArray.length; i++) {
-        if(str.toLowerCase().includes(spamArray[i])) {
-            flag = true;
-        }
-    }
-
-    return flag;
-}
-
-
-
-
-// Домашня практика
-
-/*
-Задача 3.
-
-Написати функцію яка перевіряє, чи є переданий рядок паліндромом.
-Не зважаючи на регістр.
-Паліндром - це коли рядок з обох боків читається однаково.
-
-Anna - паліндром
-Papa - не паліднром
-Namman - паліндром
-*/
-
-function palindrom(str) {
-    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
-}
-
-console.log(palindrom('NaMmAN'));
-
-
-
-/*
-Задача 4
-
-Написати функцію, яка приймає рядок і повертає кількість голосних літер, що містяться в рядку.
-(мова про латинську абетку)
-
-Голосні: ['a', 'e', 'i', 'o', 'u'];
-
-*/
-
-function countVowels(str) {
-    const array = str.toLowerCase().split('');
-    const vowelsArray = ['a', 'e', 'i', 'o', 'u'];
-    const resultArray = array.filter((letter) => vowelsArray.includes(letter));
-    return resultArray.length;
-}
-
-console.log(countVowels('innocent'));
+console.log(time2.getTime() - time1.getTime());
