f2e577ac6ebf79a11653bc10a267aae4978350ec
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-10 20:14:56 +0200

    Extends example v1

--- index.js
+++ index.js
@@ -1,55 +1,22 @@
-/*
-Напишіть клас RangeValidator
-дві властивості:
-from, to
-
-from не може бути більше to
-
-Завдання:
-1. реалізувати сеттери для обох властивостей
-2. геттер getRange, який має повертати масив цілих чисел цього діапазону
-*/
-
-class RangeValidator {
-    constructor(from, to) {
-        this.to = to; // this._to
-        this.from = from;
+class Animal {
+    constructor(color, name, tail) {
+        this.name = name;
+        this.color = color;
+        this.tail = tail;
     }
 
-    set from(value) {
-        if(typeof value !== 'number') {
-            throw new TypeError('From must be a number');
-        }
-        if(value > this._to) {
-            throw new RangeError('To must be a larger than from')
-        }
-        this._from = value;
+    eat() {
+        return `${this.name} is eating`
+    }
+}
+
+// class Dog extends Animal - клас Dog розширює клас Animal (наслідує цей клас)
+class Dog extends Animal {
+    constructor(color, name, tail) {
+        super(color, name, tail);
     }
 
-    set to(value) {
-        if(typeof value !== 'number') {
-            throw new TypeError('To must be a number');
-        }
-        this._to = value;
-    }
-
-    get to() {
-        return this._to;
-    }
-
-    get from() {
-        return this._from;
-    }
-
-    get getRange() {
-        const arr = [];
-        for(let i = this._from; i <= this._to; i++) {
-            arr.push(i);
-        }
-        return arr;
-    }
-
-    validate(num) {
-        return this.getRange.includes(num);
+    barking() {
+        return `${this.name} is barking`
     }
 }
