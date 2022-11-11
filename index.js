f0a1c92213e3ecca3a711bae724929336b7c4d6b
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-11 19:08:22 +0200

    Class Figure

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
-class User {
-    constructor(name, surname, age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+class Figure {
+    constructor(sideQuantity) {
+        this.sideQuantity = sideQuantity;
     }
 
-    getFullName() {
-        return `${this.name} ${this.surname}`;
+    getArea() {
+
     }
 }
 
-class Moderator extends User {
-    constructor(name, surname, age) {
-        super(name, surname, age);
+class Triangle extends Figure {
+    constructor(a, b, angle) {
+        super(3);
+        this.a = a;
+        this.b = b;
+        this.angle = angle;
     }
 
-    createPost(header, text) {
-        console.log('Create post')
+    get a() {
+        return this._a;
     }
 
-    deletePost(index) {
-        console.log('Delete post')
+    set a(value) {
+        if(value < 0) {
+            throw new RangeErrro('Side can`t be less than 0');
+        }
+        this._a = value;
+    }
+
+    get b() {
+        return this._b;
+    }
+
+    set b(value) {
+        if(value < 0) {
+            throw new RangeErrro('Side can`t be less than 0');
+        }
+        this._b = value;
+    }
+
+    getArea() {
+        return this.a * this.b * Math.sin(this.angle);
     }
 }
 
-class Admin extends Moderator {
-    constructor(name, surname, isAdmin) {
-        super(name, surname, null);
-        this.isAdmin = isAdmin;
+class Square extends Figure {
+    constructor(a) {
+        super(4);
+        this.a = a;
     }
 
-    setModerator() {
-        console.log('Set moderator')
+    get a() {
+        return this._a;
     }
 
-    deleteModerator() {
-        console.log('Delete moderator')
+    set a(value) {
+        if(value < 0) {
+            throw new RangeErrro('Side can`t be less than 0');
+        }
+        this._a = value;
     }
+
+    getArea() {
+        return this.a * this.a;
+    } 
 }
+
+// Написати аналогічний клас Circle, який буде мати метод для обчислення площі та буде інкапсулювати дані (аксессори)
