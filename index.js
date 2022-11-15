f1ec7ce32b0f4cef2fc57fb5fc46185d39717a0c
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-15 20:19:36 +0200

    Stack

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-const arr = [2, 4, 1, 3, 7, 3, 2, 1, 3, 5, 2, 8, 2, 4, 1];
+/* Stack */
 
-function linearSearch(arr, value) { // лінійна складність
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === value) {
-            return i;
-        }
+class Stack {
+    constructor(maxSize) {
+        this._maxSize = maxSize;
+        this._size = 0;
     }
 
-    return -1;
+    get isEmpty() {
+        return this._size === 0;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    get maxSize() {
+        return this._maxSize;
+    }
+
+    set size(value) {
+        throw new Error('Access denied');
+    }
+
+    set maxSize(value) {
+        throw new Error('Access denied');
+    }
+
+    push(value) {
+        if(this.size >= this.maxSize) {
+            throw new Error('Stack overflow');
+        }
+        this[`_${this.size}`] = value;
+        this._size++;
+        return this.size;
+    }
+
+    pop() {
+        if(this.size > 0) {
+            const lastItem = this[`_${this.size - 1}`];
+            delete this[`_${this.size - 1}`];
+            this._size--;
+            return lastItem;
+        } 
+    }
+
+    pick() {
+        return this[`_${this.size - 1}`]
+    }
 }
 
-function multyTable(limit) { // квадратична складність (парабола)
-    const table = [];
-    for(let i = 1; i <= limit; i++) {
-        for(let j = 1; j <= limit; j++) {
-            table.push(`${i} * ${j} = ${i*j}`)
-        }
-    }
-
-    return table;
-}
-
-const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-function binarySearch(array, whatToFind) {
-    if(whatToFind > array[array.length-1]) {
-        return -Infinity;
-    }
-
-    let start = 0;
-    let end = array.length-1;
-    let middle = Math.round(start + end / 2);
-
-    while(true) {
-        if(array[middle] === whatToFind) {
-            return middle;
-        }
-        if(array[middle] < whatToFind) {
-            start = middle;
-            middle = Math.ceil(start + end / 2);
-        } else {
-            end = middle;
-            middle = Math.ceil(start + end / 2);
-        }
-    }
-}
+const myStack = new Stack(4);
