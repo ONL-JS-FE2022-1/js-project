946de6f8b6563253bd39bab8b448da5ce28dbd63
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-16 20:23:39 +0200

    Map with example

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-class Queue {
-    constructor(...args) {
-        this._head = 0;
-        this._tail = 0;
-        for(const item of args) {
-            this.enqueue(item);
+/*
+Map (Карта, Мапа)
+
+Зберігає пари ключ-значення
+Має розмір (size)
+
+Відмінності від звичайного об'єкта:
+1. Запам'ятовує елементи в тому порядку, в якому ми їх додаємо.
+2. Ключем може бути будь-який тип даних: String, Number, Boolean, BigInt, Function, Object
+(Об'єкти і функція передаються за посиланням)
+
+Ключ має бути унікальним
+*/
+
+const vocalbuary = new Map();
+
+vocalbuary.set('cat', 'кіт');
+vocalbuary.set('dog', 'собака');
+vocalbuary.set('eat', 'їсти');
+
+function translater(str, vocalbuary) {
+    const arrayWords = str.toLowerCase().trim().split(' ');
+
+    const translatedArray = arrayWords.map((word) => {
+        if(vocalbuary.has(word)) {
+            return vocalbuary.get(word);
         }
-    }
+        return word;
+    })
 
-    get size() {
-        return this._tail - this._head;
-    }
-
-    enqueue(value) { // вставляє елемент в кінець черги
-        this[this._tail] = value;
-        this._tail++;
-        return this.size;
-    }
-
-    dequeue() { // видаляє елемент з початку черги
-        if(this.size > 0) {
-            const lastItem = this[this._head];
-            delete this[this._head++];
-            return lastItem;
-        }
-    }
+    return translatedArray.join(' ');
 }
