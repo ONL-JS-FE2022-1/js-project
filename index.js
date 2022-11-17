8005fde88663289bf7a37e8ed477f1f475ea4a89
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-17 21:12:37 +0200

    Destructure of objects

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
-/*
-Set - множина (множество)
-набір унікальних значень
-*/
+/* Деструктуризація */
 
-/*
-Дан масив: [2,3,3,4,6,6,6,6,7,7,7,7,2,2,2,2,1,1,1]
-Повернути масив без повторень (Set)
-*/
-
-/* варіант простіше
-function arrWithoutDoubles(arr) {
-    const set = new Set(arr);
-    const arrFromSet = [...set.values()];
-    return arrFromSet;
+const monitor = {
+    sizes: {
+        width: {
+            value: 30,
+            scale: 'cm'
+        },
+        height: {
+            value: 60,
+            scale: 'cm'
+        }
+    },
+    brightness: 750,
+    refresh: {
+        value: 144,
+        scale: 'Hnz'
+    },
+    color: 'black',
+    resolution: '4K'
 }
-*/
 
-const arrWithoutDoubles = arr => [...new Set(arr)];
+monitor.color; // black => color
+monitor.sizes.height; // height => object
+monitor.sizes.height.value; // значення висоти
+
+// const height = monitor.sizes.height.value;
+
+
+// const {resolution, color: monitorColor} = monitor;
+const {sizes: {height: {value: heightValue}, width: {value: widthValue}}} = monitor;
+// heightValue, widthValue - звичайні змінні
+
+const {color, brightness, resolution, ...restOfMonitor} = monitor;
+
+
+
+
+
+
+
+
+
+
+
+
+
+const test1 = {
+    test2: {
+        test3: {
+            test4: {
+                test5: {
+                    test6: {
+                        test7: {
+                            test8: {
+                                test9: {
+                                    test10: {
+                                        value: 'test'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
